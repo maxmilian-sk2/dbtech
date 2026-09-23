a62781fe7fd3d89e75897869bec7a8af0ccb0134
maxmilian-sk2 <maxmilian.kohacek@student.ukf.sk>
2026-09-23 12:49:45 +0100

    Add "create_tables" script + csv data

---
+++ 01_create_tables.sql
@@ -0,0 +1,35 @@
+-- Active: 1790162515282@@127.0.0.1@5432@superstore
+-- Active: 1790162515282@@127.0.0.1@5432@Northwind
+
+
+CREATE DATABASE superstore;
+
+CREATE TABLE customers (
+    customer_id VARCHAR(20) PRIMARY KEY,
+    customer_name VARCHAR(100),
+    segment VARCHAR(50),
+    country VARCHAR(50),
+    region VARCHAR(50)
+);
+
+CREATE TABLE products (
+    product_id VARCHAR(20) PRIMARY KEY,
+    category VARCHAR(50),
+    sub_category VARCHAR(50),
+    product_name VARCHAR(100)
+);
+
+CREATE TABLE orders (
+    order_id VARCHAR(20) PRIMARY KEY,
+    customer_id VARCHAR(20),
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+    product_id VARCHAR(20),
+    FOREIGN KEY (product_id) REFERENCES products(product_id),
+    order_date DATE,
+    ship_date DATE,
+    sales DECIMAL(10,2),
+    quantity INT,
+    discount DECIMAL(10,2),
+    profit DECIMAL(10,2)
+);
+
