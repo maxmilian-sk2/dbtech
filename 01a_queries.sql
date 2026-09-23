fc1a1914b49c336001a6f1ff4f16f23e37b2c0e9
maxmilian-sk2 <maxmilian.kohacek@student.ukf.sk>
2026-09-23 13:17:46 +0100

    Add files via upload
    Region sales select

--- 01a_queries.sql
+++ 01a_queries.sql
@@ -7,3 +7,5 @@ SELECT * FROM orders;
 SELECT orders.order_id, customers.customer_name, orders.sales FROM orders JOIN customers ON orders.customer_id = customers.customer_id WHERE orders.sales > 500;
 
 SELECT orders.order_id, customers.customer_name, products.category, orders.sales FROM orders JOIN customers ON orders.customer_id = customers.customer_id JOIN products ON orders.product_id = products.product_id;
+
+SELECT customers.region, SUM(orders.sales) FROM orders RIGHT JOIN customers on orders.customer_id = customers.customer_id GROUP BY customers.region;
