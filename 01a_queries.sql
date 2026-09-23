f3b0818072706280221a9f326ab815e8265b4668
maxmilian-sk2 <maxmilian.kohacek@student.ukf.sk>
2026-09-23 13:12:11 +0100

    Add new select

--- 01a_queries.sql
+++ 01a_queries.sql
@@ -5,3 +5,5 @@ SELECT * FROM products;
 SELECT * FROM orders;
 
 SELECT orders.order_id, customers.customer_name, orders.sales FROM orders JOIN customers ON orders.customer_id = customers.customer_id WHERE orders.sales > 500;
+
+SELECT orders.order_id, customers.customer_name, products.category, orders.sales FROM orders JOIN customers ON orders.customer_id = customers.customer_id JOIN products ON orders.product_id = products.product_id;
